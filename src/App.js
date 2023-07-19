62bb1eb4000f2b7dd86bc9991a6010baf809ca4f
rajesh <rajeshrakee66@gmail.com>
2023-07-19 07:53:54 +0000

    authentication functionality

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {Switch, Route} from 'react-router-dom'
+
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './components/Home'
+import Login from './components/Login'
+import About from './components/About'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <Switch className="container">
+    <ProtectedRoute exact path="/" component={Home} />
+    <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/about" component={About} />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
